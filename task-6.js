6518939f113e076105596467c92ee78459b7cd25
unknown <R4z>
2023-04-20 12:05:13 +0300

    added answers to tasks

---
+++ task-6.js
@@ -0,0 +1,8 @@
+var total = 0;
+var input = 0;
+do {
+    total += parseInt(input);
+    input = prompt("Введiть число", "");
+} while (input);
+
+console.log(total);
---
+++ task-8.js
@@ -0,0 +1,6 @@
+const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+
+const firstTwoEls = fruits.slice(0, 2);
+const nonExtremeEls = fruits.slice(2, fruits.length);
+const lastTreeEls = fruits.slice(fruits.length - 3, fruits.length);
