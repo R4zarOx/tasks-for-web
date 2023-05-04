6a65a9df5128a453f2f85018e33ee3d48e2ebb98
unknown <R4z>
2023-05-04 13:12:01 +0300

    revorked solution

--- task-10.js
+++ task-10.js
@@ -1,9 +1,9 @@
-const formatString = function (string)
+function formatString(string)
 {
     if (string.length <= 40)
         return string;    
     else
-        return string.slice(0, 40)
+        return string.slice(0, 40) + "..."
 };
 
 console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
